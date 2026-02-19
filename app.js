a412e2298aac4e0ee8bd22c59104e226cb5be8f8
anaayaar-ops <ana.ayaar@gmail.com>
2026-02-19 15:51:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const { WOLF } = wolfjs;
 const settings = {
     identity: process.env.U_MAIL,
     secret: process.env.U_PASS,
-    targetBotId: 51660277, 
-    actionWord: "صيد",
+    targetBotId: 45578849, 
+    actionWord: "!بطل 5",
     delayBetweenHeists: 11000,      // 11 ثانية فاصل بين الصيد
     workDuration: 54 * 60 * 1000,   // 54 دقيقة عمل
     restDuration: 6 * 60 * 1000     // 6 دقائق راحة
