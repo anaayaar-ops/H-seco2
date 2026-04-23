b75ef92bb1a61268e9d202c1c033da98a526fba0
anaayaar-ops <ana.ayaar@gmail.com>
2026-04-23 16:02:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const settings = {
     identity: process.env.U_MAIL,
     secret: process.env.U_PASS,
     targetBotId: 76305584 , 
-    actionWord: "!صياد اوروبا ٣",
+    actionWord: "!صياد اسيا 3",
     delayBetweenHeists: 11000,      // 11 ثانية فاصل بين الصيد
     workDuration: 54 * 60 * 1000,   // 54 دقيقة عمل
     restDuration: 6 * 60 * 1000     // 6 دقائق راحة
